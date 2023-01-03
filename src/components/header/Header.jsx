77fbffeb0709e94a408c20f77829faf8ea59a3f0
sushantgb <sgbharat.joint@gmail.com>
2023-01-03 17:00:34 +0530

    1 file modified

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Search from './Search';
 //import { NavLink } from 'react-router-dom';
+import { useGlobalContext } from '../../context/Context';
 const Header = () => {
-
+  const [contextVal, setContextVal] = useGlobalContext();
   return (
     <>
       <div className="header-container">
         <div>
-          <button onClick={()=> {window.location.reload()}}>Gallery</button>
+          <button onClick={()=> setContextVal("All")}>Gallery</button>
           {/* <NavLink className="home-btn" to="/refresh">
             <h4>Gallery</h4>
           </NavLink> */}
