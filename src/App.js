a5e3351e057b81e253d46cbc0ceab388c009ba86
sushantgb <sgbharat.joint@gmail.com>
2023-01-03 16:54:18 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useContext } from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 import PostsList from './components/posts/PostsList';
 import Header from './components/header/Header';
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export default function App() {
-
   return (
     <div>
-      <Header />
-      <PostsList />
+      <BrowserRouter>
+        <Header />
+        <Routes forceRefresh>
+          <Route exact path="/" element={< PostsList/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Search from './Search';
-
+//import { NavLink } from 'react-router-dom';
 const Header = () => {
+
   return (
     <>
       <div className="header-container">
         <div>
-          <h4>Gallery</h4>
+          <button onClick={()=> {window.location.reload()}}>Gallery</button>
+          {/* <NavLink className="home-btn" to="/refresh">
+            <h4>Gallery</h4>
+          </NavLink> */}
         </div>
         <div>
           <Search />
