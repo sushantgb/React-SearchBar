98baebebdde63ce65cf232df392d953a04e41090
sushantgb <sgbharat.joint@gmail.com>
2023-01-04 15:00:28 +0530

    1 file modified

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useGlobalContext } from '../../context/Context';
 
 const Search = () => {
   const [contextValue, setContextValue] = useGlobalContext();
-  console.log("context value : " + contextValue);
+  /* search field state to clear it*/
+  const [searchState, setSearchState] = useState();
+  console.log('context value : ' + contextValue);
   /* triggering search on enter button */
   const SearchFunc = (e) => {
-    if(e.key === 'Enter'){
-      const searchValue = e.target.value
+    if (e.key === 'Enter') {
+      const searchValue = e.target.value;
       setContextValue(searchValue.toLowerCase());
+      setSearchState('');
     }
-  }
-  return(
+  };
+  return (
     <>
       <div className="search-div">
-        <input 
-        type='text' 
-        placeholder="Search and Enter"
-        onKeyPress={(e) => SearchFunc(e)}
-        />      
+        <input
+          type="text"
+          value={searchState}
+          placeholder="Search and Enter"
+          onKeyPress={(e) => SearchFunc(e)}
+          onChange={(e) => setSearchState(e.target.value)}
+        />
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Search;
